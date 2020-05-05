5a8541c020d38b8dd6c8ce2d80bdf9c609d64052
alfonso <alfonso@THINKS>
2020-05-05 16:09:20 +0200

    aggiornamento parziale di una risorsa e ricerca

--- src/main/java/it/tss/projectwork/users/UserStore.java
+++ src/main/java/it/tss/projectwork/users/UserStore.java
@@ -7,8 +7,8 @@ package it.tss.projectwork.users;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
@@ -19,32 +19,48 @@ import javax.enterprise.context.ApplicationScoped;
  */
 @ApplicationScoped
 public class UserStore {
-    
-    private final Map<Long,User> users = new HashMap<>();
-    
+
+    private final Map<Long, User> users = new HashMap<>();
+
     @PostConstruct
-    public void init(){
+    public void init() {
         Stream.of(new User(1l, "rossi", "rossipwd"), new User(2l, "verdi", "verdipwd"), new User(3l, "bianchi", "bianchipwd"))
                 .forEach(v -> users.put(v.getId(), v));
     }
-    
-    public Collection<User> all(){
+
+    public Collection<User> all() {
         return users.values();
     }
-    
-    public User create(User u){
+
+    public User find(Long id) {
+        return users.get(id);
+    }
+
+    public User create(User u) {
         System.out.println("create user " + u);
         users.putIfAbsent(u.getId(), u);
         return users.get(u.getId());
     }
-    
-    public User update(User u){
+
+    public User update(User u) {
         System.out.println("update user " + u);
-        return users.put(u.getId(), u);
+        users.put(u.getId(), u);
+        return users.get(u.getId());
     }
-    
-    public void delete(Long id){
+
+    public void delete(Long id) {
         System.out.println("delete user " + id);
         users.remove(id);
+    }
+
+    public Collection<User> search(String search) {
+        return users.values().stream()
+                .filter(v -> this.search(v,search)).collect(Collectors.toList());
+    }
+    
+    private boolean search(User u, String search){
+        return  (u.getFirstName()!= null && u.getFirstName().contains(search))
+                || (u.getLastName()!= null && u.getLastName().contains(search)) 
+                || (u.getUsr()!= null && u.getUsr().contains(search));
     }
 }
--- src/main/java/it/tss/projectwork/users/UsersResource.java
+++ src/main/java/it/tss/projectwork/users/UsersResource.java
@@ -7,12 +7,18 @@ package it.tss.projectwork.users;
 
 import java.util.Collection;
 import javax.inject.Inject;
+import javax.json.JsonObject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -21,24 +27,31 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("/users")
 public class UsersResource {
-    
+
     @Inject
     UserStore store;
-    
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Collection<User> all(){
-        return store.all();
+    public Collection<User> all(@QueryParam("search") String search) {
+        return search== null  ? store.all() : store.search(search);
     }
-    
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User find(@PathParam("id") Long id) {
+        return store.find(id);
+    }
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public User create(User u){
+    public User create(User u) {
         User saved = store.create(u);
         return saved;
     }
-    
+
     @POST
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
@@ -52,5 +65,31 @@ public class UsersResource {
         user.setLastName(lname);
         User saved = store.create(user);
         return saved;
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public User update(@PathParam("id") Long id, User u) {
+        if (u.getId() == null || !u.getId().equals(id)) {
+            throw new IllegalArgumentException("risorsa con id non valido");
+        }
+        return store.update(u);
+    }
+
+    @PATCH
+    @Path("{id}/firstname")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User updateFirstName(@PathParam("id") Long id, JsonObject json) {
+        User found = store.find(id);
+        found.setFirstName(json.getString("firstName"));
+        return store.update(found);
+    }
+
+    @DELETE
+    @Path("{id}/{nome}")
+    public void delete(@PathParam("id") Long id) {
+        store.delete(id);
     }
 }
